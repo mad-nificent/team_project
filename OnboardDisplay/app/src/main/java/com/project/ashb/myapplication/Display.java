f14e5ff4b9c3615665f58a0f3b2b85265efdd974
AshBrindle <ash.brindle97@gmail.com>
2019-12-02 21:03:09 +0000

    Updating Values
    Updates and displays values dynamically, the indicators have also been implemented

--- OnboardDisplay/app/src/main/java/com/project/ashb/myapplication/Display.java
+++ OnboardDisplay/app/src/main/java/com/project/ashb/myapplication/Display.java
@@ -14,7 +14,10 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -30,21 +33,21 @@ public class Display extends AppCompatActivity {
 
     TextView textview_battery;
     TextView textview_speed;
+    TextView textview_indicator_left;
+    TextView textview_indicator_right;
     BluetoothGatt gatt;
 
     BluetoothDevice device;
 
     String SERVICE_UUID = "dee0e505-9680-430e-a4c4-a225905ce33d"; // iPad Peripheral
-    //String SERVICE_UUID = "0000180f-0000-1000-8000-00805f9b34fb";   // BLExplorer
     String BATTERY_LEVEL_CHARACTERISTIC_UUID = "76a247fb-a76f-42da-91ce-d6a5bdebd0e2";  // iPad Peripheral
     String SPEED_LEVEL_CHARACTERISTIC_UUID = "7b9b53ff-5421-4bdf-beb0-ca8c949542c1";  // iPad Peripheral
+    String INDICATOR_CHARACTERISTIC_UUID = "74df0c8f-f3e1-4cf5-b875-56d7ca609a2e";
     String DESCRIPTOR_UUID = "00002902-0000-1000-8000-00805f9b34fb";
-    //String BATTERY_LEVEL_CHARACTERISTIC_UUID = "00002a19-0000-1000-8000-00805f9b34fb";  // BLExplorer
 
     Packet packet = new Packet();
     List<BluetoothGattDescriptor> desc = new ArrayList<>();
     List<BluetoothGattCharacteristic> chars = new ArrayList<>();
-    boolean finished = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,11 +58,12 @@ public class Display extends AppCompatActivity {
         Intent intent = getIntent();
         device = intent.getExtras().getParcelable("device");
 
-        //gatt = device.connectGatt(getApplicationContext(), false, gatt_callback);
         gatt = device.connectGatt(getApplicationContext(), false, gatt_callback, 2);
 
         textview_battery = (TextView) findViewById(R.id.text_battery);
         textview_speed = (TextView) findViewById(R.id.text_speed);
+        textview_indicator_left = (TextView) findViewById(R.id.text_indicator_left);
+        textview_indicator_right = (TextView) findViewById(R.id.text_indicator_right);
     }
 
     public BluetoothGattCallback gatt_callback = new BluetoothGattCallback() {
@@ -120,6 +124,7 @@ public class Display extends AppCompatActivity {
             BluetoothGattService service = gatt.getService(UUID.fromString(SERVICE_UUID));
             BluetoothGattCharacteristic batteryLevel = service.getCharacteristic(UUID.fromString(BATTERY_LEVEL_CHARACTERISTIC_UUID));
             BluetoothGattCharacteristic speedLevel = service.getCharacteristic(UUID.fromString(SPEED_LEVEL_CHARACTERISTIC_UUID));
+            BluetoothGattCharacteristic indicator = service.getCharacteristic(UUID.fromString(INDICATOR_CHARACTERISTIC_UUID));
 
             List<BluetoothGattCharacteristic> gattchars = service.getCharacteristics();
             Log.d("onCharDiscovered", "Char count: " + gattchars.size());
@@ -139,22 +144,26 @@ public class Display extends AppCompatActivity {
 
             chars.add(batteryLevel);
             chars.add(speedLevel);
+            chars.add(indicator);
 
             gatt.setCharacteristicNotification(batteryLevel, true);
             gatt.setCharacteristicNotification(speedLevel, true);
+            gatt.setCharacteristicNotification(indicator, true);
 
             desc.add(batteryLevel.getDescriptor(UUID.fromString(DESCRIPTOR_UUID)));
             desc.add(speedLevel.getDescriptor(UUID.fromString(DESCRIPTOR_UUID)));
+            desc.add(indicator.getDescriptor(UUID.fromString(DESCRIPTOR_UUID)));
 
             desc.get(0).setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
             desc.get(1).setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+            desc.get(2).setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
 
-            setNotifications(0);
+            setNotifications();
 
         }
 
-        public void setNotifications(int pos) {
-            gatt.writeDescriptor(desc.get(pos));
+        public void setNotifications() {
+            gatt.writeDescriptor(desc.get(desc.size()-1));
         }
 
         public void readChars() {
@@ -180,6 +189,13 @@ public class Display extends AppCompatActivity {
                     Log.d(TAG, "Error (read)" + e);
                 }
             }
+            else if (characteristic.getUuid().toString().equals(INDICATOR_CHARACTERISTIC_UUID)){
+                try {
+                    packet.indicator = new String(characteristic.getValue(), StandardCharsets.UTF_8);
+                } catch (Exception e) {
+                    Log.d(TAG, "Error (read)" + e);
+                }
+            }
             try {
                 runGUIThread();
             } catch (Exception e) {
@@ -187,7 +203,6 @@ public class Display extends AppCompatActivity {
             }
 
             chars.remove(chars.get(chars.size() - 1));
-
             if (chars.size() > 0) {
                 readChars();
             }
@@ -198,11 +213,11 @@ public class Display extends AppCompatActivity {
         public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
             super.onDescriptorWrite(gatt, descriptor, status);
             Log.d(TAG, "onDescriptorWrite :" + ((status == BluetoothGatt.GATT_SUCCESS) ? "Sucess" : "false"));
-            if (!finished) {
-                setNotifications(1);
-                finished = true;
+            desc.remove(desc.get(desc.size() - 1));
+            if (desc.size() > 0) {
+                setNotifications();
             }
-            if (finished) {
+            else {
                 readChars();
             }
         }
@@ -215,6 +230,9 @@ public class Display extends AppCompatActivity {
             }
             else if (characteristic.getUuid().toString().equals(SPEED_LEVEL_CHARACTERISTIC_UUID)) {
                 packet.speed_level = new String(characteristic.getValue(), StandardCharsets.UTF_8);
+            }
+            else if (characteristic.getUuid().toString().equals(INDICATOR_CHARACTERISTIC_UUID)) {
+                packet.indicator = new String(characteristic.getValue(), StandardCharsets.UTF_8);
             }
             try {
                 runGUIThread();
@@ -234,6 +252,18 @@ public class Display extends AppCompatActivity {
                         public void run() {
                             textview_battery.setText(packet.battery_level);
                             textview_speed.setText(packet.speed_level);
+                            if (packet.indicator.equals("Right")) {
+                                textview_indicator_right.setVisibility(View.VISIBLE);
+                                textview_indicator_left.setVisibility(View.INVISIBLE);
+                            }
+                            else if (packet.indicator.equals("Left")) {
+                                textview_indicator_left.setVisibility(View.VISIBLE);
+                                textview_indicator_right.setVisibility(View.INVISIBLE);
+                            }
+                            else if (packet.indicator.equals("None")) {
+                                textview_indicator_left.setVisibility(View.INVISIBLE);
+                                textview_indicator_right.setVisibility(View.INVISIBLE);
+                            }
                         }
                     });
                     Thread.sleep(300);
--- OnboardDisplay/app/src/main/java/com/project/ashb/myapplication/Packet.java
+++ OnboardDisplay/app/src/main/java/com/project/ashb/myapplication/Packet.java
@@ -3,4 +3,5 @@ package com.project.ashb.myapplication;
 public class Packet {
     public String battery_level = "-1";
     public String speed_level = "-1";
+    public String indicator = "None";
 }
